75468f6a76f753dbd070f5fb8d96a2e0c3b4cdca
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-07 20:47:01 +0300

    feat: create login

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import MainComponent from './pages/main/main.component';
 import RegistrationComponent from './pages/registration/registration.component';
+import LoginComponent from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, RegistrationComponent],
+  imports: [RouterOutlet, MainComponent, RegistrationComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
