340a0b25061f83731c7f63a73f3190401aa4418c
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-07 20:51:46 +0300

    feat: create 404 page

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import MainComponent from './pages/main/main.component';
 import RegistrationComponent from './pages/registration/registration.component';
 import LoginComponent from './pages/login/login.component';
+import NotFoundComponent from './pages/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, RegistrationComponent, LoginComponent],
+  imports: [RouterOutlet, MainComponent, RegistrationComponent, LoginComponent, NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
