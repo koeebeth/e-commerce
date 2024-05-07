661f8ebda81a7c9c330834b93ddc805b81a64c0f
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-07 20:43:20 +0300

    feat: create registration

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import MainComponent from './pages/main/main.component';
+import RegistrationComponent from './pages/registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, MainComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- e-commerce/src/app/pages/registration/registration.component.spec.ts
+++ e-commerce/src/app/pages/registration/registration.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import MainComponent from './main.component';
+import RegistrationComponent from './registration.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('RegistrationComponent', () => {
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent],
+      imports: [RegistrationComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
