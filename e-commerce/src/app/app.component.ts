8f0cc286476695af1cc3eab12730d3d1542f05d0
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-07 20:34:41 +0300

    fix: update export

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainComponent } from './pages/main/main.component';
+import MainComponent from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
