c51c60534471eb3fe023c463d974f1ed695871fe
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-10 18:42:54 +0300

    refactor: remove auth component

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import MainComponent from './pages/main/main.component';
 import NotFoundComponent from './pages/not-found/not-found.component';
-import AuthComponent from './pages/auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, AuthComponent, NotFoundComponent],
+  imports: [RouterOutlet, RouterLink, MainComponent, NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
