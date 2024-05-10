6ca7f3fcf00a307bb9977db40988a32992b89f5d
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-10 18:57:25 +0300

    fix: delete some imports

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import MainComponent from './pages/main/main.component';
-import NotFoundComponent from './pages/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MainComponent, NotFoundComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
