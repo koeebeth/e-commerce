a49b91bbfa71fad59a99312dc10e60691e641eaa
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-14 15:22:29 +0300

    fix: change input component to accept formgroup from parent

--- e-commerce/src/app/pages/login/login-form/login-form.component.ts
+++ e-commerce/src/app/pages/login/login-form/login-form.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import InputComponent from '../../../shared/components/input/input.component';
+import ButtonComponent from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [ReactiveFormsModule, InputComponent],
+  imports: [ReactiveFormsModule, InputComponent, ButtonComponent],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
-  inputForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  loginForm!: FormGroup;
   emailValidation = {
     pattern: {
       regex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
       errorMsg: 'Email should be in format abc.123@example.com',
     },
+    required: true,
   };
   passwordValidation = {
     minlength: 8,
@@ -26,5 +25,19 @@ export class LoginFormComponent {
       regex: /((?=[^ ]*\d)(?=[^ ]*[a-z])(?=[^ ]*[A-Z])(?=[^ ]*[\W])[^ ]+)/g,
       errorMsg: 'Password must contain at least one uppercase letter, lowercase letter, number and a special character',
     },
+    required: true,
   };
+  isValid = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({});
+  }
+
+  onKeyup() {
+    if (this.loginForm.valid) this.isValid = true;
+    else this.isValid = false;
+    console.log(this.loginForm);
+  }
 }
