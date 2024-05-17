22fb5918871422dc29dd95e9c036b359bc0a71ae
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-17 13:42:12 +0300

    feat: add custom checkbox component

---
+++ e-commerce/src/app/shared/components/checkbox-input/checkbox-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox-input.component.html',
+  styleUrl: './checkbox-input.component.scss',
+})
+export class CheckboxInputComponent {
+  @Input() form!: FormGroup;
+
+  @Input() name: string = '';
+
+  @Input() label: string = '';
+
+  @Input() checked: boolean = false;
+
+  @Output() checkHandler = new EventEmitter();
+
+  control!: FormControl;
+
+  ngOnInit() {
+    this.control = new FormControl('');
+    this.form.addControl(this.name, this.control);
+  }
+
+  handleCheck() {
+    this.checkHandler.emit();
+  }
+}
