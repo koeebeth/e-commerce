c946159ed936fc88000fa5f3f5f69781b6d7e199
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-17 14:42:41 +0300

    fix: fix name validation

--- e-commerce/src/app/pages/registration/registration-form/registration-validators.ts
+++ e-commerce/src/app/pages/registration/registration-form/registration-validators.ts
@@ -22,7 +22,7 @@ const RegistrationValidators = {
   nameValidation: {
     required: true,
     pattern: {
-      regex: /^[a-zA-Z\-]+[^\-]$/,
+      regex: /^[a-zA-Z\-]*[a-zA-Z]+$/,
       errorMsg: 'Name should contain only letters and hyphens and end with a letter',
     },
   },
