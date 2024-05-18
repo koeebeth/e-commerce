a764f370c930d802d8a8b3c34d7ce5f5b7b7b300
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-18 23:24:17 +0300

    fix: change router

--- e-commerce/src/app/shared/components/header/header.component.ts
+++ e-commerce/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import BurgerMenuComponent from './burger-menu/burger-menu.component';
 import ProfileAuthorizedComponent from './profile-authorized/profile-authorized.component';
 import ProfileUnauthorizedComponent from './profile-unauthorized/profile-unauthorized.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
