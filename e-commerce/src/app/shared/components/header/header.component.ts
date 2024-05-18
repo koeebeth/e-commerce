93eab1c2c7caa2210a467b4b535f7f4ffabd8892
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-18 23:23:19 +0300

    feat: add a link to main

--- e-commerce/src/app/shared/components/header/header.component.ts
+++ e-commerce/src/app/shared/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import BurgerMenuComponent from './burger-menu/burger-menu.component';
 import ProfileAuthorizedComponent from './profile-authorized/profile-authorized.component';
 import ProfileUnauthorizedComponent from './profile-unauthorized/profile-unauthorized.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BurgerMenuComponent, ProfileAuthorizedComponent, ProfileUnauthorizedComponent],
+  imports: [RouterLink, BurgerMenuComponent, ProfileAuthorizedComponent, ProfileUnauthorizedComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
