b72767ed74d7ec442bd14f0fe71e8a7e9c180a1a
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-18 21:13:47 +0300

    feat: create slider

--- e-commerce/src/app/pages/main/slider/slider.component.ts
+++ e-commerce/src/app/pages/main/slider/slider.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NgxFlickingModule, Plugin } from '@egjs/ngx-flicking';
+import { AutoPlay, Fade, Pagination } from '@egjs/flicking-plugins';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [NgxFlickingModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
-export default class SliderComponent {}
+export default class SliderComponent {
+  plugins: Plugin[] = [
+    new AutoPlay({ duration: 5000, direction: 'NEXT', stopOnHover: false }),
+    new Pagination({ type: 'bullet' }),
+    new Fade(),
+  ];
+}
