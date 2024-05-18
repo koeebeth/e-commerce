db1e8777020ca7ed9af2ad2ed7e7f498686fbd6a
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2024-05-18 23:04:22 +0300

    feat/Notification Component (#15)
    * feat: create notification component
    
    * feat: create service for notification
    
    * feat: implement auto close notification
    
    * feat: add style
    
    * fix: change opening notification
    
    * feat: update icon
    
    * feat: implement queue
    
    * feat: change icon
    
    * feat: add delay
    
    * feat: update background
    
    * fix: handle the wrong types
    
    * fix: fix cash

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -2,20 +2,29 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import CommerceApiService from './shared/services/commercetoolsApi/commercetoolsapi.service';
 import SharedModule from './shared/shared.module';
+import NotificationComponent from './shared/components/notification/notification.component';
+import { NotificationService } from './shared/services/notification/notification.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, RouterLink],
+  imports: [RouterOutlet, SharedModule, RouterLink, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export default class AppComponent {
   title = 'e-commerce';
 
-  constructor(private commerceApiService: CommerceApiService) {}
+  constructor(
+    private commerceApiService: CommerceApiService,
+    private notificationService: NotificationService,
+  ) {}
 
   ngOnInit() {
     this.commerceApiService.refreshAccessToken();
+    // usage example:
+    this.notificationService.showNotification('warning', 'Warning text');
+    this.notificationService.showNotification('error', 'Error text');
+    this.notificationService.showNotification('success', 'Success text');
   }
 }
