e57cc6b47cb41234acb4ec26383d98eb52890d5b
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-20 01:23:50 +0300

    feat: implement request to the store for header

--- e-commerce/src/app/pages/registration/registration.component.ts
+++ e-commerce/src/app/pages/registration/registration.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import ButtonComponent from '../../shared/components/button/button.component';
 
-
 @Component({
   selector: 'app-registration',
   standalone: true,
--- e-commerce/src/app/shared/components/header/header.component.ts
+++ e-commerce/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../store/store';
 import BurgerMenuComponent from './burger-menu/burger-menu.component';
 import ProfileAuthorizedComponent from './profile-authorized/profile-authorized.component';
 import ProfileUnauthorizedComponent from './profile-unauthorized/profile-unauthorized.component';
@@ -7,8 +11,16 @@ import ProfileUnauthorizedComponent from './profile-unauthorized/profile-unautho
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, BurgerMenuComponent, ProfileAuthorizedComponent, ProfileUnauthorizedComponent],
+  imports: [CommonModule, RouterLink, BurgerMenuComponent, ProfileAuthorizedComponent, ProfileUnauthorizedComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export default class HeaderComponent {}
+export default class HeaderComponent {
+  authToken$!: Observable<string>;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.authToken$ = this.store.select((state) => state.app.accessToken);
+  }
+}
