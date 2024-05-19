904613c3f8a66d122b5065ae5f2de1553894ba8f
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-19 14:35:58 +0300

    refactor: update slider

--- e-commerce/src/app/pages/main/slider/slider.component.ts
+++ e-commerce/src/app/pages/main/slider/slider.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NgxFlickingModule, Plugin } from '@egjs/ngx-flicking';
 import { AutoPlay, Fade, Pagination } from '@egjs/flicking-plugins';
+import { CommonModule } from '@angular/common';
 import '@egjs/flicking-plugins/dist/pagination.css';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [NgxFlickingModule],
+  imports: [NgxFlickingModule, CommonModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
@@ -15,5 +16,11 @@ export default class SliderComponent {
     new AutoPlay({ duration: 5000, direction: 'NEXT', stopOnHover: false }),
     new Pagination({ type: 'bullet' }),
     new Fade(),
+  ];
+
+  images = [
+    { src: '../../../../assets/Enshrouded.jpg', alt: 'Enshrouded' },
+    { src: '../../../../assets/banishers.jpeg', alt: 'Banishers' },
+    { src: '../../../../assets/BALDURS_GATE.jpeg', alt: 'Baldurs Gate' },
   ];
 }
