f97109489cf215a7d0acd441ff6942e9e4383f83
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2024-05-19 15:56:33 +0300

    feat/Main Layout (#17)
    * feat: add category block
    
    * feat: change style of category
    
    * feat: add main img
    
    * fix: add alt text
    
    * feat: do adaptive
    
    * feat: create slider
    
    * fix: change pagination
    
    * refactor: update category
    
    * refactor: update slider
    
    * fix: fix cash

---
+++ e-commerce/src/app/pages/main/slider/slider.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NgxFlickingModule, Plugin } from '@egjs/ngx-flicking';
+import { AutoPlay, Fade, Pagination } from '@egjs/flicking-plugins';
+import { CommonModule } from '@angular/common';
+import '@egjs/flicking-plugins/dist/pagination.css';
+
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [NgxFlickingModule, CommonModule],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss',
+})
+export default class SliderComponent {
+  plugins: Plugin[] = [
+    new AutoPlay({ duration: 5000, direction: 'NEXT', stopOnHover: false }),
+    new Pagination({ type: 'bullet' }),
+    new Fade(),
+  ];
+
+  images = [
+    { src: '../../../../assets/Enshrouded.jpg', alt: 'Enshrouded' },
+    { src: '../../../../assets/banishers.jpeg', alt: 'Banishers' },
+    { src: '../../../../assets/BALDURS_GATE.jpeg', alt: 'Baldurs Gate' },
+  ];
+}
