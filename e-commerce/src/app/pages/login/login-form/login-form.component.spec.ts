2c63dd0e15096d1d3ff6ae7dc2c1c8cc960916db
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-22 15:33:24 +0300

    feat: add login tests

--- e-commerce/src/app/pages/login/login-form/login-form.component.spec.ts
+++ e-commerce/src/app/pages/login/login-form/login-form.component.spec.ts
@@ -29,5 +29,27 @@ describe('LoginFormComponent', () => {
     expect(emailInput.valid).withContext('with empty email').toBe(false);
     emailInput.setValue('aBc@example.com');
     expect(emailInput.valid).withContext('with uppercase letters').toBe(false);
+    emailInput.setValue('ab..c@example.com');
+    expect(emailInput.valid).withContext('with multiple periods').toBe(false);
+    emailInput.setValue('abc@example.com');
+    expect(emailInput.valid).withContext('with valid email').toBe(true);
+  });
+
+  it('should validate password', () => {
+    const form = component.loginForm;
+    const passwordInput = form.controls['password'];
+    expect(passwordInput.valid).withContext('with empty password').toBe(false);
+  });
+
+  it('should toggle password', () => {
+    const passwordInput = fixture.nativeElement.querySelector('input[name="password"]');
+    const showBtn = fixture.nativeElement.querySelector('.login-form-password button');
+    expect(passwordInput.type).withContext('should hide on init').toBe('password');
+    showBtn.click();
+    fixture.detectChanges();
+    expect(passwordInput.type).withContext('should show on click').toBe('text');
+    showBtn.click();
+    fixture.detectChanges();
+    expect(passwordInput.type).withContext('should hide on click').toBe('password');
   });
 });
