00e2ecf74ed1e11aa49c3be904ece0cfa96de1b4
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2024-05-22 13:14:32 +0300

    feat/Testing Main Page and Not-Found Page (#35)
    * feat: add test for not found page
    
    * feat: add test for main component

--- e-commerce/src/app/pages/main/main.component.spec.ts
+++ e-commerce/src/app/pages/main/main.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import MainComponent from './main.component';
 
 describe('MainComponent', () => {
@@ -18,5 +18,21 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display categories', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const categoryElements = compiled.querySelectorAll('.category-name');
+    expect(categoryElements.length).toBe(component.categories.length);
+    component.categories.forEach((category, index) => {
+      expect(categoryElements[index].textContent).toContain(category.categoryName);
+    });
+  });
+
+  it('should display category images with correct alt tags', () => {
+    const imgElements = fixture.debugElement.queryAll(By.css('.category-img'));
+    imgElements.forEach((img, index) => {
+      expect(img.properties['alt']).toBe(component.categories[index].alt);
+    });
   });
 });
--- e-commerce/src/app/pages/not-found/not-found.component.spec.ts
+++ e-commerce/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import NotFoundComponent from './not-found.component';
 
 describe('NotFoundComponent', () => {
@@ -9,6 +9,12 @@ describe('NotFoundComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NotFoundComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotFoundComponent);
@@ -18,5 +24,15 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display correct error message', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.error-message').textContent).toContain('Looks like you got lost');
+  });
+
+  it('should display correct error text', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.error-text').textContent).toContain('Come back and try again');
   });
 });
