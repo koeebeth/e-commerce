533d72c2fb95d77b82b51d3f1e830a825e219bc8
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-23 21:33:08 +0300

    feat: add profile page link

--- e-commerce/src/app/app.routes.ts
+++ e-commerce/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import LoginComponent from './pages/login/login.component';
 import RegistrationComponent from './pages/registration/registration.component';
 import MainComponent from './pages/main/main.component';
 import NotFoundComponent from './pages/not-found/not-found.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [
   { path: 'main', title: 'CyberVault', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'profile', component: ProfilePageComponent },
   { path: '404', component: NotFoundComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
