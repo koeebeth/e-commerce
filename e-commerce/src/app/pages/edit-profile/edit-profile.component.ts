ccb1ec39fd429a6093ba26afaea00a62d7e2d1b8
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-27 15:22:07 +0300

    refactor: move validators to shared folder

--- e-commerce/src/app/pages/edit-profile/edit-profile.component.ts
+++ e-commerce/src/app/pages/edit-profile/edit-profile.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router, RouterLink } from '@angular/router';
 import { AppState } from '../../store/store';
+import InputComponent from '../../shared/components/input/input.component';
+import ButtonComponent from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, InputComponent, ButtonComponent],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss',
 })
