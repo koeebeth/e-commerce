e2629d51fb3054346419022dd7c360515afa5b86
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-27 15:37:55 +0300

    feat: add edit page layout

--- e-commerce/src/app/pages/edit-profile/edit-profile.component.ts
+++ e-commerce/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,20 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Router, RouterLink } from '@angular/router';
 import { AppState } from '../../store/store';
+import RegistrationValidators from '../../shared/utils/registration-validators';
 import InputComponent from '../../shared/components/input/input.component';
 import ButtonComponent from '../../shared/components/button/button.component';
+import SelectInputComponent from '../../shared/components/select-input/select-input.component';
+import CheckboxInputComponent from '../../shared/components/checkbox-input/checkbox-input.component';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [RouterLink, InputComponent, ButtonComponent],
+  imports: [
+    RouterLink,
+    InputComponent,
+    ButtonComponent,
+    CommonModule,
+    SelectInputComponent,
+    CheckboxInputComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss',
 })
 export class EditProfileComponent {
+  editForm!: FormGroup;
+
+  emailValidation = RegistrationValidators.emailValidation;
+
+  passwordValidation = RegistrationValidators.passwordValidation;
+
+  nameValidation = RegistrationValidators.nameValidation;
+
+  streetValidation = RegistrationValidators.streetValidation;
+
+  cityValidation = RegistrationValidators.cityValidation;
+
+  ageValidation = RegistrationValidators.ageValidation;
+
+  usPostalcodeValidation = RegistrationValidators.usPostalcodeValidation;
+
+  ukPostalcodeValidation = RegistrationValidators.ukPostalcodeValidation;
+
+  countryList = ['United States', 'United Kingdom', 'Germany'];
+
+  countryIsoFormat = [
+    { contry: 'United States', iso: 'US' },
+    { contry: 'United Kingdom', iso: 'GB' },
+    { contry: 'Germany', iso: 'DE' },
+  ];
+
   constructor(
+    private fb: FormBuilder,
     private store: Store<AppState>,
     private router: Router,
   ) {}
+
+  ngOnInit() {
+    this.editForm = this.fb.group({});
+  }
+
+  onSubmit() {
+    console.log(this.editForm.value);
+  }
 }
