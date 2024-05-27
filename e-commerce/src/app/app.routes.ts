7bc3a1dc21a6a487ca105d96efa906d908890ac1
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-27 18:15:02 +0300

    fix: delete routes

--- e-commerce/src/app/app.routes.ts
+++ e-commerce/src/app/app.routes.ts
@@ -3,14 +3,12 @@ import LoginComponent from './pages/login/login.component';
 import RegistrationComponent from './pages/registration/registration.component';
 import MainComponent from './pages/main/main.component';
 import NotFoundComponent from './pages/not-found/not-found.component';
-import ProductComponent from './pages/product/product.component';
 
 const routes: Routes = [
   { path: 'main', title: 'CyberVault', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: '404', component: NotFoundComponent },
-  { path: 'product', component: ProductComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
