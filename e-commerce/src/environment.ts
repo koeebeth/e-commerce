e06984e3e2a40152658d8e35972c27ed8c2d0a4b
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-27 22:20:08 +0300

    refactor: change environment.ts

--- e-commerce/src/environment.ts
+++ e-commerce/src/environment.ts
@@ -1,12 +1,12 @@
 export const authVisitorAPI = {
   production: false,
   ctpProjectKey: 'tt-e-commerce',
-  ctpClientSecret: 'BdbraSQOLZxLq_PYuUT95lnM05HwmEbW',
-  ctpClientId: 'elDZJoJydTml9dHxOUY2dE_d',
-  ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-  ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
+  ctpClientSecret: 'fWWRPWoeB23V2VRL2iNArN6JRWRoiFAH',
+  ctpClientId: 'kuTVsAbhnrTcJxhZUTuahe4A',
+  ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com/',
+  ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com/',
   ctpScopes:
-    'view_tax_categories:tt-e-commerce view_payments:tt-e-commerce view_stores:tt-e-commerce view_types:tt-e-commerce manage_my_orders:tt-e-commerce manage_my_profile:tt-e-commerce manage_my_shopping_lists:tt-e-commerce view_cart_discounts:tt-e-commerce view_attribute_groups:tt-e-commerce manage_shipping_methods:tt-e-commerce view_categories:tt-e-commerce view_published_products:tt-e-commerce manage_my_payments:tt-e-commerce view_orders:tt-e-commerce view_discount_codes:tt-e-commerce',
+    'view_tax_categories:tt-e-commerce view_payments:tt-e-commerce view_stores:tt-e-commerce view_types:tt-e-commerce manage_my_orders:tt-e-commerce manage_my_shopping_lists:tt-e-commerce view_cart_discounts:tt-e-commerce view_attribute_groups:tt-e-commerce manage_shipping_methods:tt-e-commerce view_categories:tt-e-commerce view_published_products:tt-e-commerce manage_my_payments:tt-e-commerce manage_my_profile:tt-e-commerce view_products:tt-e-commerce view_orders:tt-e-commerce view_discount_codes:tt-e-commerce',
 };
 export const unauthVisitorAPI = {
   production: false,
