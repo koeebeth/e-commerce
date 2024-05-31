cfab6b83f783a2e4a9c956aac8c49e5e6fbaa5b0
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-05-31 14:28:38 +0300

    fix: fix styling and variables

--- e-commerce/src/app/app.routes.ts
+++ e-commerce/src/app/app.routes.ts
@@ -3,16 +3,12 @@ import LoginComponent from './pages/login/login.component';
 import RegistrationComponent from './pages/registration/registration.component';
 import MainComponent from './pages/main/main.component';
 import NotFoundComponent from './pages/not-found/not-found.component';
-import EditProfileComponent from './pages/edit-profile/edit-profile.component';
 import ProfilePageComponent from './pages/profile-page/profile-page.component';
-import ChangePasswordComponent from './pages/change-password/change-password.component';
 
 const routes: Routes = [
   { path: 'main', title: 'CyberVault', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: 'edit-profile', component: EditProfileComponent },
-  { path: 'change-password', component: ChangePasswordComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: '404', component: NotFoundComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
