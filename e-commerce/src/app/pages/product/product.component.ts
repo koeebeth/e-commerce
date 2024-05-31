2c05dce7ab7b6318f35c274b897d4bd4a9276040
Angelina Bezik <angelina1337bz@gmail.com>
2024-05-31 15:32:18 +0300

    feat: add prices

--- e-commerce/src/app/pages/product/product.component.ts
+++ e-commerce/src/app/pages/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { register } from 'swiper/element/bundle';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -11,7 +12,7 @@ import { Product, ProductPagedQueryResponse } from '../../shared/services/produc
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [SliderComponent],
+  imports: [SliderComponent, CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
@@ -25,6 +26,24 @@ export default class ProductComponent {
   name: string = '';
 
   description: string = '';
+
+  centAmount: number = 0;
+
+  fractionDigits: number = 0;
+
+  discountCentAmount: number = 0;
+
+  discountFractionDigits: number = 0;
+
+  price!: string;
+
+  originalPrice!: number;
+
+  discuntedPrice!: number;
+
+  discountPercnt!: number;
+
+  currency!: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -47,7 +66,52 @@ export default class ProductComponent {
               this.description = this.product?.masterData?.current?.description['en-US'] || '';
             }
           }
+          this.getOriginalPrice();
+          this.getDiscuntedPrice();
+          this.formatPrice();
+          this.getDiscountProcentage();
         });
     });
+  }
+
+  getDiscuntedPrice() {
+    const discounts = this.product?.masterData?.current?.masterVariant?.prices;
+    const priceObj = discounts?.find(
+      (d) => d.discounted?.value?.centAmount != null && d.discounted?.value?.fractionDigits != null,
+    );
+
+    if (priceObj && priceObj.discounted?.value) {
+      this.discountCentAmount = priceObj.discounted.value.centAmount ?? 0;
+      this.discountFractionDigits = priceObj.discounted.value.fractionDigits ?? 2;
+    }
+  }
+
+  getOriginalPrice() {
+    const prices = this.product?.masterData?.current?.masterVariant?.prices;
+    const priceObj = prices?.find((p) => p.value?.centAmount != null && p.value?.fractionDigits != null);
+
+    if (priceObj && priceObj.value) {
+      this.centAmount = priceObj.value.centAmount ?? 0;
+      this.fractionDigits = priceObj.value.fractionDigits ?? 2;
+    }
+  }
+
+  formatPrice() {
+    this.originalPrice = this.centAmount / 10 ** this.fractionDigits;
+    this.discuntedPrice = this.discountCentAmount / 10 ** this.discountFractionDigits;
+    this.currency =
+      this.product?.masterData?.current?.masterVariant?.prices?.find((c) => c.value?.currencyCode != null)?.value
+        ?.currencyCode ?? 'USD';
+  }
+
+  getDiscountProcentage() {
+    const originalPrice = this.centAmount / 10 ** this.fractionDigits;
+    const discuntedPrice = this.discountCentAmount / 10 ** this.discountFractionDigits;
+
+    if (originalPrice !== 0 && discuntedPrice !== 0) {
+      this.discountPercnt = parseFloat((((originalPrice - discuntedPrice) / originalPrice) * 100).toFixed(0));
+    } else {
+      this.discountPercnt = 0;
+    }
   }
 }
