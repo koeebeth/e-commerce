ab7cb4a37d7ac1ed42b4cd84f471398e0c10367c
Angelina Bezik <angelina1337bz@gmail.com>
2024-06-01 00:20:16 +0300

    feat: add redirect to catalog

--- e-commerce/src/app/store/effects.ts
+++ e-commerce/src/app/store/effects.ts
@@ -259,7 +259,15 @@ export default class EcommerceEffects {
           switchMap(([anonToken, accessToken]) =>
             this.productsService.getProductById(action.id, accessToken || anonToken).pipe(
               map((product: Product) => actions.loadProductIdSuccess({ product })),
-              catchError((error) => of(actions.loadProductIdFailure({ error: error.message }))),
+              catchError((error) => {
+                this.router.navigate(['/catalog']);
+                this.notificationService.showNotification('error', 'The Product was not found');
+                return of(
+                  actions.loadProductIdFailure({
+                    error: error.message,
+                  }),
+                );
+              }),
             ),
           ),
         ),
