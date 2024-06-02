8b00bbb8f769592711cdb01817b50824a50e9531
Angelina Bezik <angelina1337bz@gmail.com>
2024-06-02 16:11:22 +0300

    feat: implement slider

--- e-commerce/src/app/pages/product/image-modal/image-modal.component.ts
+++ e-commerce/src/app/pages/product/image-modal/image-modal.component.ts
@@ -1,18 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SwiperContainer } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-image-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './image-modal.component.html',
   styleUrl: './image-modal.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export default class ImageModalComponent {
   @Input() images: string[] | undefined = [];
 
   @Input() selectedImage: string = '';
 
+  @Input() name: string = '';
+
   @Output() closeModalEvent = new EventEmitter<void>();
+
+  @ViewChild('swiperContainer', { static: true }) swiperContainer!: ElementRef<SwiperContainer>;
+
+  initialSlide: number = 0;
+
+  ngOnInit(): void {
+    const selectedImageIndex = this.images?.indexOf(this.selectedImage);
+    this.initialSlide = selectedImageIndex ?? 0;
+    const swiperOptions: SwiperOptions = {
+      initialSlide: this.initialSlide,
+    };
+    Object.assign(this.swiperContainer.nativeElement, swiperOptions);
+    this.swiperContainer.nativeElement.initialize();
+  }
 
   close() {
     this.closeModalEvent.emit();
