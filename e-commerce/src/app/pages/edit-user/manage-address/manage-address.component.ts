20370b8153e17de7bbead1f16321546a0dfdf1ba
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-06-03 23:11:17 +0300

    fix: fix linter errors

--- e-commerce/src/app/pages/edit-user/manage-address/manage-address.component.ts
+++ e-commerce/src/app/pages/edit-user/manage-address/manage-address.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
