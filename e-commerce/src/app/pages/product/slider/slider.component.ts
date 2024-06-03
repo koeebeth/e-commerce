fdc91e941be52298604db98d9f5daf114cc78a4b
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2024-06-03 17:34:32 +0300

    feat/Enlarged Image Modal (#47)
    * feat: create a modal with img
    
    * feat: implement slider
    
    * feat: update design

--- e-commerce/src/app/pages/product/slider/slider.component.ts
+++ e-commerce/src/app/pages/product/slider/slider.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import ImageModalComponent from '../image-modal/image-modal.component';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageModalComponent],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -13,4 +14,16 @@ export default class SliderComponent {
   @Input() images: string[] | undefined = [];
 
   @Input() name: string = '';
+
+  selectedImage: string = '';
+
+  openModal(image: string) {
+    this.selectedImage = image;
+    document.body.classList.add('modal-open');
+  }
+
+  closeModal() {
+    this.selectedImage = '';
+    document.body.classList.remove('modal-open');
+  }
 }
