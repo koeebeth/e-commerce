31c878ac270f647680ccd6b27a84394dd3f5c711
MaruyYak <maryana.lus@gmail.com>
2024-06-12 15:56:54 +0300

    feat: create footer

--- e-commerce/src/app/pages/about-us/about-us.component.ts
+++ e-commerce/src/app/pages/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-interface devsInfo {
+interface DevsInfo {
   name: string;
-  photo: string;
+  photoAI: string;
+  photoOrig: string;
   icon: string;
   text: string;
   link: string;
@@ -19,10 +20,11 @@ interface devsInfo {
   styleUrl: './about-us.component.scss',
 })
 export default class AboutUsComponent {
-  devsInfo: devsInfo[] = [
+  devsInfo: DevsInfo[] = [
     {
       name: 'Maryana',
-      photo: '../../../assets/photo_2023.jpg',
+      photoAI: '../../../assets/avatars/maryana_ai.jpg',
+      photoOrig: '../../../assets/avatars/maryana_orig.jpg',
       icon: '🧐',
       text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
       gitLogo: '../../../assets/github.svg',
@@ -31,21 +33,27 @@ export default class AboutUsComponent {
     },
     {
       name: 'Angelina',
-      photo: '../../../assets/photo_2023.jpg',
+      photoAI: '../../../assets/avatars/maryana_ai.jpg',
+      photoOrig: '../../../assets/avatars/maryana_orig.jpg',
       icon: '🤓',
       text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
       gitLogo: '../../../assets/github.svg',
       role: 'Front-End Developer',
-      link: 'https://github.com/MaruyYak',
+      link: 'https://github.com/angelinabz',
     },
     {
       name: 'Liza',
-      photo: '../../../assets/photo_2023.jpg',
+      photoAI: '../../../assets/avatars/maryana_ai.jpg',
+      photoOrig: '../../../assets/avatars/maryana_orig.jpg',
       icon: '😎',
       text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
       gitLogo: '../../../assets/github.svg',
       role: 'Front-End Developer',
-      link: 'https://github.com/MaruyYak',
+      link: 'https://github.com/koeebeth',
     },
   ];
+
+  ngOnInit(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
