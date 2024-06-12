680bd4df9e7d239b6384ec944df1c4fbb2758eba
MaruyYak <maryana.lus@gmail.com>
2024-06-12 21:00:55 +0300

    feat: add responsive design

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -5,7 +5,7 @@ import CommerceApiService from './shared/services/commercetoolsApi/commercetools
 import SharedModule from './shared/shared.module';
 import NotificationComponent from './shared/components/notification/notification.component';
 import HeaderComponent from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import FooterComponent from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
