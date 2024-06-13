9a511ab5cf2951db0f54b47d5e6d0439ae7bbaca
koeebeth <96339397+koeebeth@users.noreply.github.com>
2024-06-13 13:37:47 +0300

    feat: add empty cart page

--- e-commerce/src/app/pages/cart/cart.component.ts
+++ e-commerce/src/app/pages/cart/cart.component.ts
@@ -1,28 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import ButtonComponent from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, ButtonComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
-  products = [
-    {
-      name: 'Game',
-      price: '1.00',
-      quantity: '1',
-    },
-    {
-      name: 'Game',
-      price: '1.00',
-      quantity: '1',
-    },
-    {
-      name: 'Game',
-      price: '1.00',
-      quantity: '1',
-    },
-  ];
+  products = [];
 }
