c8490cbf056e43ef2c7c2c9ef7c58941a49160be
MaruyYak <maryana.lus@gmail.com>
2024-06-18 05:40:37 +0300

    refactor: update about

--- e-commerce/src/app/pages/about-us/about-us.component.ts
+++ e-commerce/src/app/pages/about-us/about-us.component.ts
@@ -33,8 +33,8 @@ export default class AboutUsComponent {
     },
     {
       name: 'Angelina',
-      photoAI: '../../../assets/avatars/maryana_ai.jpg',
-      photoOrig: '../../../assets/avatars/maryana_orig.jpg',
+      photoAI: '../../../assets/avatars/angelina_ai.jpg',
+      photoOrig: '../../../assets/avatars/angelina_orig.jpg',
       icon: '🤓',
       text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
       gitLogo: '../../../assets/github.svg',
@@ -43,8 +43,8 @@ export default class AboutUsComponent {
     },
     {
       name: 'Liza',
-      photoAI: '../../../assets/avatars/maryana_ai.jpg',
-      photoOrig: '../../../assets/avatars/maryana_orig.jpg',
+      photoAI: '../../../assets/avatars/liza_ai.jpg',
+      photoOrig: '../../../assets/avatars/liza_orig.jpg',
       icon: '😎',
       text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
       gitLogo: '../../../assets/github.svg',
