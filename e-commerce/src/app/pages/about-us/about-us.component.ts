6d186f98d2989e43738236d7bfc91a939c2e3fee
MaruyYak <maryana.lus@gmail.com>
2024-06-18 15:49:33 +0300

    feat: updated about page

--- e-commerce/src/app/pages/about-us/about-us.component.ts
+++ e-commerce/src/app/pages/about-us/about-us.component.ts
@@ -6,7 +6,8 @@ interface DevsInfo {
   photoAI: string;
   photoOrig: string;
   icon: string;
-  text: string;
+  biography: string;
+  contribution: string;
   link: string;
   role: string;
   gitLogo: string;
@@ -26,7 +27,10 @@ export default class AboutUsComponent {
       photoAI: '../../../assets/avatars/maryana_ai.jpg',
       photoOrig: '../../../assets/avatars/maryana_orig.jpg',
       icon: '🧐',
-      text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.',
+      biography:
+        'Marianna is a former business analyst, consequently, she assumed the role of team leader. Her skills in requirements management and experience working with development teams in an agile environment helped in process mapping and enabled her to develop efficient workflows.',
+      contribution:
+        'Maryana configured CommerceTools processes and was responsible for managing the CommerceTools platform, including implementing server requests to retrieve key data such as tokens, logins, registrations, products, and more. She also developed the catalog product page with filters, sorting, pagination options, and an About page, and implemented promo code functionality. Quickly mastering complex tasks such as server configuration, she also led the implementation of creative ideas and solutions.',
       gitLogo: '../../../assets/github.svg',
       role: 'Front-End Developer',
       link: 'https://github.com/MaruyYak',
@@ -36,7 +40,10 @@ export default class AboutUsComponent {
       photoAI: '../../../assets/avatars/angelina_ai.jpg',
       photoOrig: '../../../assets/avatars/angelina_orig.jpg',
       icon: '🤓',
-      text: 'Angelina graduated from the University with a degree in information technology in economics. Since then, she has worked as a master-data analyst. Angelina describes herself as an analytical-minded and disciplined person. So in this project we organized the work in the way that each team member could accomplish various tasks, thereby we learned more different things. As a part of this project Angelina completed tasks such as configuring the project repository, creating layouts for the application pages, routing between pages, creating a detailed product page, writing requests to add and remove items from the cart, updating the Store and much more.',
+      biography:
+        'Angelina graduated from the University with a degree in information technology in economics. Since then, she has worked as a master-data analyst. Angelina describes herself as an analytical-minded and disciplined person.',
+      contribution:
+        'As a part of this project Angelina completed tasks such as configuring the project repository, creating layouts for the application pages, routing between pages, creating a detailed product page, implementing sliders and zoom functionality on this page, writing requests to add and remove items from the Cart, updating the Store and much more. She is a master of code attention to detail, she not only writes clean, error-free code but also diligently notices and raises concerns about potential issues.',
       gitLogo: '../../../assets/github.svg',
       role: 'Front-End Developer',
       link: 'https://github.com/angelinabz',
@@ -46,7 +53,10 @@ export default class AboutUsComponent {
       photoAI: '../../../assets/avatars/liza_ai.jpg',
       photoOrig: '../../../assets/avatars/liza_orig.jpg',
       icon: '😎',
-      text: 'Liza started learning programming after graduating from high school, before starting Computer Science studies. She is generally seen as a flexible person and a quick thinker. In creating of this project she was responsible for creating reusable components and forms, designing and implementing some of the pages like profile and cart pages, writing some of the service and store methods etc.',
+      biography:
+        'Liza started learning programming after graduating from high school, before starting Computer Science studies. She is generally seen as a flexible person and a quick thinker.',
+      contribution:
+        'In creating of this project she was responsible for creating key and reusable components and forms for Login and Registration pages. She implemented validation functionality for these forms and designed and implemented pages such as Categories, Profile and Cart. Additionally, she wrote complex service and store methods for the Profile page, handling tasks such as updating user data and addresses. Also developed the main functionality for the Cart page. Liza is a guru in writing concise, understandable, and readable reusable code.',
       gitLogo: '../../../assets/github.svg',
       role: 'Front-End Developer',
       link: 'https://github.com/koeebeth',
