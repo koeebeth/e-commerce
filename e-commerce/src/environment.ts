5452bbfe67ff3771872f1bdc0c0a36ef7e49b733
Angelina Bezik <angelina1337bz@gmail.com>
2025-05-26 14:22:25 +0300

    fix: update api info

--- e-commerce/src/environment.ts
+++ e-commerce/src/environment.ts
@@ -1,41 +1,41 @@
 export const authVisitorAPI = {
   production: false,
-  ctpProjectKey: 'game-store',
-  ctpClientId: 'bRidiRtr_bu2PjhQUDFg8j2s',
-  ctpClientSecret: 'DSvk_vOAf-9jJBOzKZ8skbqNVMdhWRse',
+  ctpProjectKey: 'new-game-store',
+  ctpClientId: 'ag3chZM2LGV_lNdRbexMcPh3',
+  ctpClientSecret: 'xWfn8xMQqxsveG7ysA3rUO0MNvNe8DeN',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
   ctpScopes:
-    'view_cart_discounts:game-store view_types:game-store manage_orders:game-store manage_my_shopping_lists:game-store view_orders:game-store manage_my_orders:game-store manage_my_payments:game-store manage_shipping_methods:game-store view_attribute_groups:game-store view_discount_codes:game-store view_categories:game-store view_stores:game-store view_published_products:game-store view_products:game-store manage_my_profile:game-store view_tax_categories:game-store view_payments:game-store',
+    'manage_my_shopping_lists:new-game-store view_types:new-game-store view_tax_categories:new-game-store manage_shipping_methods:new-game-store view_categories:new-game-store view_discount_codes:new-game-store manage_my_profile:new-game-store manage_my_payments:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store view_payments:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_attribute_groups:new-game-store',
 };
 export const unauthVisitorAPI = {
   production: false,
-  ctpProjectKey: 'game-store',
-  ctpClientId: 'fnGrXhNQ7i6NWILXlCsrfUC1',
-  ctpClientSecret: 'Rs4yVJcqbN0Wg7tBNqgLbkrJHD8v6NeX',
+  ctpProjectKey: 'new-game-store',
+  ctpClientId: 'N7HNYVGMqzHOTR6N1kR-Lo2Z',
+  ctpClientSecret: 'QVbO6EawkaABuLk_NRSN9fRwD03qMWJR',
   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
   ctpScopes:
-    'view_cart_discounts:game-store view_messages:game-store view_types:game-store view_shopping_lists:game-store manage_customers:game-store manage_orders:game-store manage_my_shopping_lists:game-store manage_my_orders:game-store view_standalone_prices:game-store create_anonymous_token:game-store view_categories:game-store view_stores:game-store view_published_products:game-store view_products:game-store manage_my_profile:game-store view_tax_categories:game-store',
+    'manage_my_shopping_lists:new-game-store view_standalone_prices:new-game-store view_types:new-game-store manage_customers:new-game-store view_tax_categories:new-game-store create_anonymous_token:new-game-store view_categories:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_shopping_lists:new-game-store view_messages:new-game-store manage_my_profile:new-game-store',
 };
 // export const authVisitorAPI = {
 //   production: false,
-//   ctpProjectKey: 'games-store',
-//   ctpClientId: 'HptA07venZOGfIbdilx9Y34G',
-//   ctpClientSecret: 'R2dw2WR6mcrYLfJ2kIsPp7LCP205f-8o',
+//   ctpProjectKey: 'game-store',
+//   ctpClientId: 'bRidiRtr_bu2PjhQUDFg8j2s',
+//   ctpClientSecret: 'DSvk_vOAf-9jJBOzKZ8skbqNVMdhWRse',
 //   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
 //   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
 //   ctpScopes:
-//     'view_cart_discounts:games-store view_types:games-store manage_orders:games-store manage_my_shopping_lists:games-store view_orders:games-store manage_my_orders:games-store manage_my_payments:games-store manage_shipping_methods:games-store view_attribute_groups:games-store view_discount_codes:games-store view_categories:games-store view_stores:games-store view_published_products:games-store view_products:games-store manage_my_profile:games-store view_tax_categories:games-store view_payments:games-store',
+//     'view_cart_discounts:game-store view_types:game-store manage_orders:game-store manage_my_shopping_lists:game-store view_orders:game-store manage_my_orders:game-store manage_my_payments:game-store manage_shipping_methods:game-store view_attribute_groups:game-store view_discount_codes:game-store view_categories:game-store view_stores:game-store view_published_products:game-store view_products:game-store manage_my_profile:game-store view_tax_categories:game-store view_payments:game-store',
 // };
 
 // export const unauthVisitorAPI = {
 //   production: false,
-//   ctpProjectKey: 'games-store',
-//   ctpClientId: 'fGhlFuEp3jBarwWgFa-ISs_b',
-//   ctpClientSecret: 'sZL6ClsJYfAsCLHoE94eH4uWb7m7eae6',
-//   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
+//   ctpProjectKey: 'game-store',
+//   ctpClientId: 'fnGrXhNQ7i6NWILXlCsrfUC1',
+//   ctpClientSecret: 'Rs4yVJcqbN0Wg7tBNqgLbkrJHD8v6NeX',
 //   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+//   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
 //   ctpScopes:
-//     'view_cart_discounts:games-store view_messages:games-store view_types:games-store view_shopping_lists:games-store manage_customers:games-store manage_orders:games-store manage_my_shopping_lists:games-store manage_my_orders:games-store view_standalone_prices:games-store create_anonymous_token:games-store view_categories:games-store view_stores:games-store view_published_products:games-store view_products:games-store manage_my_profile:games-store view_tax_categories:games-store',
+//     'view_cart_discounts:game-store view_messages:game-store view_types:game-store view_shopping_lists:game-store manage_customers:game-store manage_orders:game-store manage_my_shopping_lists:game-store manage_my_orders:game-store view_standalone_prices:game-store create_anonymous_token:game-store view_categories:game-store view_stores:game-store view_published_products:game-store view_products:game-store manage_my_profile:game-store view_tax_categories:game-store',
 // };
