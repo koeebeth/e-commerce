56075851c65bfe35ae8c61e11da4abd3a1db7f63
MaruyYak <maryana.lus@gmail.com>
2025-05-31 23:35:54 +0300

    fix: api

--- e-commerce/src/environment.ts
+++ e-commerce/src/environment.ts
@@ -1,44 +1,25 @@
-//New
-// export const authVisitorAPI = {
-//   production: false,
-//   ctpProjectKey: 'new-game-store',
-//   ctpClientId: 'ag3chZM2LGV_lNdRbexMcPh3',
-//   ctpClientSecret: 'xWfn8xMQqxsveG7ysA3rUO0MNvNe8DeN',
-//   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
-//   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-//   ctpScopes:
-//     'manage_my_shopping_lists:new-game-store view_types:new-game-store view_tax_categories:new-game-store manage_shipping_methods:new-game-store view_categories:new-game-store view_discount_codes:new-game-store manage_my_profile:new-game-store manage_my_payments:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store view_payments:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_attribute_groups:new-game-store',
-// };
-// export const unauthVisitorAPI = {
-//   production: false,
-//   ctpProjectKey: 'new-game-store',
-//   ctpClientId: 'N7HNYVGMqzHOTR6N1kR-Lo2Z',
-//   ctpClientSecret: 'QVbO6EawkaABuLk_NRSN9fRwD03qMWJR',
-//   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-//   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
-//   ctpScopes:
-//     'manage_my_shopping_lists:new-game-store view_standalone_prices:new-game-store view_types:new-game-store manage_customers:new-game-store view_tax_categories:new-game-store create_anonymous_token:new-game-store view_categories:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_shopping_lists:new-game-store view_messages:new-game-store manage_my_profile:new-game-store',
-// };
-//TEst
+// New
 export const authVisitorAPI = {
   production: false,
   ctpProjectKey: 'new-game-store',
-  ctpClientId: 'DqVfa2YgGwjRSXV3mu8ucqTV',
-  ctpClientSecret: 'Xep1wYe-rWGkracftNF13C0v8sxXddCI',
-  ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+  ctpClientId: 'ag3chZM2LGV_lNdRbexMcPh3',
+  ctpClientSecret: 'xWfn8xMQqxsveG7ysA3rUO0MNvNe8DeN',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
-  ctpScopes: 'manage_project:new-game-store',
+  ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+  ctpScopes:
+    'manage_my_shopping_lists:new-game-store view_types:new-game-store view_tax_categories:new-game-store manage_shipping_methods:new-game-store view_categories:new-game-store view_discount_codes:new-game-store manage_my_profile:new-game-store manage_my_payments:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store view_payments:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_attribute_groups:new-game-store',
 };
 export const unauthVisitorAPI = {
   production: false,
   ctpProjectKey: 'new-game-store',
-  ctpClientId: 'DqVfa2YgGwjRSXV3mu8ucqTV',
-  ctpClientSecret: 'Xep1wYe-rWGkracftNF13C0v8sxXddCI',
+  ctpClientId: 'N7HNYVGMqzHOTR6N1kR-Lo2Z',
+  ctpClientSecret: 'QVbO6EawkaABuLk_NRSN9fRwD03qMWJR',
   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
-  ctpScopes: 'manage_project:new-game-store',
+  ctpScopes:
+    'manage_my_shopping_lists:new-game-store view_standalone_prices:new-game-store view_types:new-game-store manage_customers:new-game-store view_tax_categories:new-game-store create_anonymous_token:new-game-store view_categories:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_shopping_lists:new-game-store view_messages:new-game-store manage_my_profile:new-game-store',
 };
-//old
+// old
 // export const authVisitorAPI = {
 //   production: false,
 //   ctpProjectKey: 'game-store',
