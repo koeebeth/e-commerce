a766b809c3099fb7bf8d4c94e90589caee5e6f3d
Angelina Bezik <angelina1337bz@gmail.com>
2025-10-21 18:22:20 +0300

    feat: added new credentials

--- e-commerce/src/environment.ts
+++ e-commerce/src/environment.ts
@@ -2,42 +2,41 @@
 
 export const authVisitorAPI = {
   production: false,
-  ctpProjectKey: 'cyber-store',
-  ctpClientId: '8YjS_8enlXsf7LiVMdS1wmwA',
-  ctpClientSecret: '40X1W7pR_Ic5DzCIBgdrbksAhiEv_8ty',
+  ctpProjectKey: 'cyber-game-store',
+  ctpClientId: 'zXsT-v8bV4hIYz3fk6iDGukn',
+  ctpClientSecret: 'tC6PLOw03PO9f25Nnj-zvuFU5B2AV0rT',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-  ctpScopes: 'manage_project:cyber-store',
+  ctpScopes: 'manage_project:cyber-game-store',
 };
 
 export const unauthVisitorAPI = {
   production: false,
-  ctpProjectKey: 'cyber-store',
-  ctpClientId: '8YjS_8enlXsf7LiVMdS1wmwA',
-  ctpClientSecret: '40X1W7pR_Ic5DzCIBgdrbksAhiEv_8ty',
+  ctpProjectKey: 'cyber-game-store',
+  ctpClientId: 'zXsT-v8bV4hIYz3fk6iDGukn',
+  ctpClientSecret: 'tC6PLOw03PO9f25Nnj-zvuFU5B2AV0rT',
   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-  ctpScopes: 'manage_project:cyber-store',
+  ctpScopes: 'manage_project:cyber-game-store',
 };
 
 // old
 // export const authVisitorAPI = {
 //   production: false,
-//   ctpProjectKey: 'new-game-store',
-//   ctpClientId: 'ag3chZM2LGV_lNdRbexMcPh3',
-//   ctpClientSecret: 'xWfn8xMQqxsveG7ysA3rUO0MNvNe8DeN',
+//   ctpProjectKey: 'cyber-store',
+//   ctpClientId: '8YjS_8enlXsf7LiVMdS1wmwA',
+//   ctpClientSecret: '40X1W7pR_Ic5DzCIBgdrbksAhiEv_8ty',
 //   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
 //   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
-//   ctpScopes:
-//     'manage_my_shopping_lists:new-game-store view_types:new-game-store view_tax_categories:new-game-store manage_shipping_methods:new-game-store view_categories:new-game-store view_discount_codes:new-game-store manage_my_profile:new-game-store manage_my_payments:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store view_payments:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_attribute_groups:new-game-store',
+//   ctpScopes: 'manage_project:cyber-store',
 // };
+
 // export const unauthVisitorAPI = {
 //   production: false,
-//   ctpProjectKey: 'new-game-store',
-//   ctpClientId: 'N7HNYVGMqzHOTR6N1kR-Lo2Z',
-//   ctpClientSecret: 'QVbO6EawkaABuLk_NRSN9fRwD03qMWJR',
-//   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+//   ctpProjectKey: 'cyber-store',
+//   ctpClientId: '8YjS_8enlXsf7LiVMdS1wmwA',
+//   ctpClientSecret: '40X1W7pR_Ic5DzCIBgdrbksAhiEv_8ty',
 //   ctpApiUrl: 'https://api.europe-west1.gcp.commercetools.com',
-//   ctpScopes:
-//     'manage_my_shopping_lists:new-game-store view_standalone_prices:new-game-store view_types:new-game-store manage_customers:new-game-store view_tax_categories:new-game-store create_anonymous_token:new-game-store view_categories:new-game-store view_products:new-game-store view_stores:new-game-store manage_my_orders:new-game-store manage_orders:new-game-store view_cart_discounts:new-game-store view_shopping_lists:new-game-store view_messages:new-game-store manage_my_profile:new-game-store',
+//   ctpAuthUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+//   ctpScopes: 'manage_project:cyber-store',
 // };
